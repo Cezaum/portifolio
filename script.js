39630c27a4a5164d3535337c9bb1168939101377
Cezaum <cesartakumi01@gmail.com>
2023-05-22 13:25:15 -0300

    update

---
+++ script.js
@@ -0,0 +1,21 @@
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById
+    ("progresstop");
+    let progressValue = document.getElementById
+    ("progress-value");
+    let pos = document.documentElement.scrollTop;
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let calcScrollValue = Math.round((pos * 100) / calcHeight);
+    if (pos>100) {
+    scrollProgress.style.display = "grid";
+    } else {
+        scrollProgress.style.display = "none";
+    }
+
+};
+
+
+
+
+window.onscroll = calcScrollValue
+window.onload = calcScrollValue
